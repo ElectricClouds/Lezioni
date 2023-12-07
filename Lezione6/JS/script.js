a67e05ade75f79c3a3bee80dc6427a93305c73ca
RainyCloud <147819950+ElectricClouds@users.noreply.github.com>
2023-12-07 11:25:27 +0100

    Update dei file delle Lezioni

---
+++ Lezione6/JS/script.js
@@ -0,0 +1,11 @@
+// Quando uso getElementById() tra le parentesi ci va l'id dell'elemento che voglio recuperare.
+let demo = document.getElementById("demo");
+
+// Voglio scrivere all'interno di demo.
+// demo.innerHTML = "Ciao Nicolò.";
+
+demo.innerHTML = "<h2> Ciao Nicolò. </h2>";
+
+let feedback = document.getElementById("feedback");
+
+feedback.innerHTML = "<h2> Ciao Christian. </h2>";
